894443ca8d144c5273f6ec6beec70a8f5cd7592e
Praveen Choudhary <pc852180@gmail.com>
2025-10-30 17:31:22 +0530

    Type Script , Mui and Tailwind Css

--- Client/src/Pages/Button.tsx
+++ Client/src/Pages/Button.tsx
@@ -5,6 +5,7 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import { Typography } from '@mui/material';
 
 function Button() {
   return (
@@ -24,6 +25,10 @@ function Button() {
         <FavoriteIcon />
       </Fab>
     </Box>
+    <Typography variant="h6" style={{fontStyle:"italic"}} sx={{color:"red"}} component="h2">
+  this is materiyal ui
+</Typography>
+ 
 
     </>
   )
